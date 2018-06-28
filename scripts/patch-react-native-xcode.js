c45b1cfaa1959d263e5e9ec83823ba3564d4af38
runoshun <runoshun@gmail.com>
2018-06-28 20:21:52 +0900

    Add patch script for react-native-xcode.sh

---
+++ scripts/patch-react-native-xcode.js
@@ -0,0 +1,28 @@
+const JsDiff = require("diff");
+const fs = require("fs");
+const path = require("path");
+
+const scriptFilePath = path.join(
+    __dirname,
+    "..",
+    "node_modules",
+    "react-native",
+    "scripts",
+    "react-native-xcode.sh"
+);
+const patchFilePath = path.join(
+    __dirname,
+    "react-native-xcode.sh.patch"
+);
+
+let source = fs.readFileSync(scriptFilePath).toString();
+let patch = fs.readFileSync(patchFilePath).toString();
+let patched = JsDiff.applyPatch(source, patch);
+
+if (patched) {
+    fs.writeFileSync(scriptFilePath, patched);
+    process.on("exit", () => process.exit(0));
+} else {
+    console.warn("failed to apply patch to react-native-xcode.sh");
+    process.on("exit", () => process.exit(0));
+}
