93aec328ad584b02c533bdda6eb9c03271dabe96
runoshun <runoshun@gmail.com>
2018-07-04 21:56:49 +0900

    Add firebase pod for ios build

---
+++ ios/Pods/Target Support Files/Pods-tsukurioki-tvOSTests/Pods-tsukurioki-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_tsukurioki_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_tsukurioki_tvOSTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-tsukuriokiTests/Pods-tsukuriokiTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_tsukuriokiTests : NSObject
+@end
+@implementation PodsDummy_Pods_tsukuriokiTests
+@end
