ad5a38b5cc9433dd60f0d127e71ef55dbd953f36
runoshun <runoshun@gmail.com>
2018-07-06 20:49:38 +0900

    Remove react-native-fs

--- ios/Pods/Target Support Files/Pods-tsukurioki-tvOSTests/Pods-tsukurioki-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_tsukurioki_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_tsukurioki_tvOSTests
-@end
--- ios/Pods/Target Support Files/Pods-tsukuriokiTests/Pods-tsukuriokiTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_tsukuriokiTests : NSObject
-@end
-@implementation PodsDummy_Pods_tsukuriokiTests
-@end
