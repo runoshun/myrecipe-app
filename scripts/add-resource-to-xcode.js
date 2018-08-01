57cedb0f79668e6f1cb84d9d5508c9123b5090c2
runoshun <runoshun@gmail.com>
2018-08-01 20:10:45 +0900

    Fix size of launchscreen image

--- scripts/add-resource-to-xcode.js
+++ scripts/add-resource-to-xcode.js
@@ -3,10 +3,10 @@ const fs = require("fs");
 const path = require("path");
 
 const PROJECT_NAME = "tsukurioki";
+const PROJECT_ROOT = path.join(__dirname, "..", "ios");
+const PROJECT_FILE_PATH = path.join(PROJECT_ROOT, `${PROJECT_NAME}.xcodeproj`, "project.pbxproj");
 
-const PROJECT_PATH = path.join(__dirname, "..", "ios", `${PROJECT_NAME}.xcodeproj`, "project.pbxproj");
-
-let proj = xcode.project(PROJECT_PATH);
+let proj = xcode.project(PROJECT_FILE_PATH);
 proj.parseSync();
 
 let rootGroup = proj.findPBXGroupKey({name: PROJECT_NAME});
@@ -14,7 +14,10 @@ let mainTarget = proj.findTargetKey(PROJECT_NAME);
 
 for(let i = 2; i < process.argv.length; ++i) {
     let file = process.argv[i];
+    if (!fs.statSync(path.join(PROJECT_ROOT, file)).isFile()) {
+        throw new Error(file + " is not exists.");
+    }
     proj.addResourceFile(file, {target: mainTarget}, rootGroup);
 }
 
-fs.writeFileSync(PROJECT_PATH, proj.writeSync());
+fs.writeFileSync(PROJECT_FILE_PATH, proj.writeSync());
